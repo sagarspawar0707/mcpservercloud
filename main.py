63a545f1978f99be6d350b1bf6f5eaf699da280d
sagarspawar0707 <sagarspawar0707@gmail.com>
2026-03-13 13:52:45 +0530

    function

--- main.py
+++ main.py
@@ -2,53 +2,46 @@ from fastmcp import FastMCP
 from datetime import datetime
 import random
 
-mcp = FastMCP("Utility MCP Server")
 
-# Tool 1
-@mcp.tool()
-def add(a: float, b: float) -> float:
-    """Add two numbers"""
-    return a + b
+def create_mcp():
+    mcp = FastMCP("Utility MCP Server")
 
+    @mcp.tool()
+    def add(a: float, b: float) -> float:
+        return a + b
 
-# Tool 2
-@mcp.tool()
-def subtract(a: float, b: float) -> float:
-    """Subtract two numbers"""
-    return a - b
+    @mcp.tool()
+    def subtract(a: float, b: float) -> float:
+        return a - b
 
+    @mcp.tool()
+    def multiply(a: float, b: float) -> float:
+        return a * b
 
-# Tool 3
-@mcp.tool()
-def multiply(a: float, b: float) -> float:
-    """Multiply two numbers"""
-    return a * b
+    @mcp.tool()
+    def divide(a: float, b: float):
+        if b == 0:
+            return "Division by zero not allowed"
+        return a / b
 
+    @mcp.tool()
+    def current_time() -> str:
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Tool 4
-@mcp.tool()
-def divide(a: float, b: float) -> float:
-    """Divide two numbers"""
-    if b == 0:
-        return "Division by zero not allowed"
-    return a / b
+    @mcp.tool()
+    def random_number(min: int, max: int) -> int:
+        return random.randint(min, max)
 
-
-# Tool 5
-@mcp.tool()
-def current_time() -> str:
-    """Get current system time"""
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-# Tool 6
-@mcp.tool()
-def random_number(min: int, max: int) -> int:
-    """Generate random number"""
-    return random.randint(min, max)
-
-def get_mcp():
     return mcp
 
+
+# expose server object
+mcp = create_mcp()
+
+
 if __name__ == "__main__":
-    mcp.run(transport='streamable-http',host="0.0.0.0",port=8000)
+    mcp.run(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=8000
+    )
